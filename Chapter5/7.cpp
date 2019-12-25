3392a5273755e458060f4a29facc54997a18c9e3
DarkT2014 <lincheng37211@gmail.com>
2019-12-25 20:49:43 +0800

    2

--- Chapter5/7.cpp
+++ Chapter5/7.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 using namespace std;
 
+int main(){
+    int rc = fork();
+    if(rc < 0) {
+        cout << "call fail" << endl;
+
+    }else if (rc == 0) {
+        cout << "i am child " << endl;
+
+    }else{
+        cout << "I am parent" << endl;
+
+    }
+}
