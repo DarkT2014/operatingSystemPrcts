aaaa7c4cca07420dffa7f11c6ddd7708265434bb
DarkT2014 <lincheng37211@gmail.com>
2020-06-14 21:14:13 +0800

    2

--- Chapter6/6_1.cpp
+++ Chapter6/6_1.cpp
@@ -10,7 +10,14 @@ using namespace std;
 
 
 int main() {
-    cout << gettimeofday() << endl;
+    struct timeval a;
+    struct timeval b;
+    gettimeofday(&a, NULL);
+    cout <<  a.tv_sec << "s, " << a.tv_usec <<"ms"<< endl;
     int f = open("./61.txt", O_RDONLY);
+    gettimeofday(&b, NULL);
+    cout <<  b.tv_sec << "s, " << b.tv_usec <<"ms"<< endl;
 
+
+    cout  << b.tv_usec - a.tv_usec <<"ms" << endl;
 }
